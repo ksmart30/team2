e737deacc1c7e4406fce5c6b2186a35443f8503c
smart-07 <smart-07@smart-07-PC>
2019-03-22 16:52:34 +0900

    [ksmart30_team2 윤예찬] 카드결재내역 입력 화면작성 <controller> PayCardController.java <view> paycard_in.html

--- ksmart30/src/main/java/com/cafe24/ksmart30/team02/account/controller/PayCardController.java
+++ ksmart30/src/main/java/com/cafe24/ksmart30/team02/account/controller/PayCardController.java
@@ -1,5 +1,12 @@
 package com.cafe24.ksmart30.team02.account.controller;
 
-public class PayCardController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class PayCardController {
+	@GetMapping("/baseinfo/paycard/in")
+	public String paycard() {
+		return "account/slip/paycard/paycard_in";
+	}
 }

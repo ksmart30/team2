11351fdd8e34bcb7b28873f6fd211dc92709ba9d
Administrator <Administrator@smart20105>
2019-03-22 17:31:16 +0900

    [team20_team2 박찬업] 전표승인화면, 미지급금 입력 화면
    detail:
    <-Controller->
    SlipAdminController.java
    UnpaidController.java
    
    <-View->
    slipadmit_in.html
    unpaid_in.html

--- ksmart30/src/main/java/com/cafe24/ksmart30/team02/account/controller/SlipAdminController.java
+++ ksmart30/src/main/java/com/cafe24/ksmart30/team02/account/controller/SlipAdminController.java
@@ -1,5 +1,12 @@
 package com.cafe24.ksmart30.team02.account.controller;
 
-public class SlipAdminController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class SlipAdminController {
+	@GetMapping("/account/slipadmit/in_form")
+	public String slipAdmitIn() {
+		return "/account/slip/admit/slipadmit_in";
+	}
 }
--- ksmart30/src/main/java/com/cafe24/ksmart30/team02/account/controller/UnpaidController.java
+++ ksmart30/src/main/java/com/cafe24/ksmart30/team02/account/controller/UnpaidController.java
@@ -1,5 +1,12 @@
 package com.cafe24.ksmart30.team02.account.controller;
 
-public class UnpaidController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class UnpaidController {
+	@GetMapping("/account/unpaid/in_form")
+	public String slipAdmitIn() {
+		return "/account/unpaid/unpaid_in";
+	}
 }

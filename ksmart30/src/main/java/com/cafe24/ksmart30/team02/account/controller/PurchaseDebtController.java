83ceb3f6a1b3e896bf5038ae4ed52ce958d95d48
윤예찬 <LineageRevolution@github.com>
2019-03-27 09:53:32 +0900

    [team20_team2 윤예찬] controller 경로 수정 및 view(html)작성
    detail:
    <-Controller->
    PurchaseDebrController.java
    <-View->
    purchase_debt.html

--- ksmart30/src/main/java/com/cafe24/ksmart30/team02/account/controller/PurchaseDebtController.java
+++ ksmart30/src/main/java/com/cafe24/ksmart30/team02/account/controller/PurchaseDebtController.java
@@ -1,5 +1,12 @@
 package com.cafe24.ksmart30.team02.account.controller;
 
-public class PurchaseDebtController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class PurchaseDebtController {
+	@GetMapping("/acc/purchasedebt/in")
+	public String purchaseDebt() {
+		return "account/slip/purchasedebt/purchase_debt";
+	}
 }
---
+++ ksmart30/src/main/java/com/cafe24/ksmart30/team02/baseinfo/controller/DepreciationController.java
@@ -0,0 +1,12 @@
+package com.cafe24.ksmart30.team02.baseinfo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DepreciationController {
+	@GetMapping("")
+	public String depriciation() {
+		return "";
+	}
+}

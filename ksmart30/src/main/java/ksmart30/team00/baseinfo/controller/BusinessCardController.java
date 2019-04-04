1b1c8eaf70805a41cf3c0c1986590beb1561a4d2
Administrator <Administrator@smart20105>
2019-04-04 10:54:08 +0900

    [Ksmart30_team2 주선태] 통장계좌,기업은행,전표승인 결로재설정 <- Controller -> BankAccountController.java BusinessCardController.java SlipAdminController.java

--- ksmart30/src/main/java/ksmart30/team00/baseinfo/controller/BusinessCardController.java
+++ ksmart30/src/main/java/ksmart30/team00/baseinfo/controller/BusinessCardController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BusinessCardController {
 	//기업카드
-	@GetMapping("/baseInfo/businesscard/in")
-	public String businessCardController() {
+	@GetMapping("/baseInfo/businessCardView")
+	public String businessCardView() {
 		System.out.println("기업카드 businessCardController ControlItemCodeController.java");
 		
 		return "baseInfo/businessCard/businessCard";

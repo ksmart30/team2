e46492444b159e25dc739be2f205735aa01e26df
Administrator <Administrator@smart20105>
2019-04-10 17:06:43 +0900

    [ksmart30_team2 주선태]전표승인 기간별검색기능만 <-Controller-> SlipAdmitController.java(전표승인 화면 기능) <-model-> slipAdmitService.java(날짜 데이터 (-) 제거,계정과목세목 코드 연결) SlipAdmit.java(컬럼추가) <-View-> SlipAdmit_in.html(전표승인 기간별검색 화면구현)

--- ksmart30/src/main/java/ksmart30/team02/account/controller/SlipAdminController.java
+++ ksmart30/src/main/java/ksmart30/team02/account/controller/SlipAdminController.java
@@ -1,13 +1,40 @@
 package ksmart30.team02.account.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import ksmart30.team02.account.domain.SlipAdmit;
+import ksmart30.team02.account.service.SlipAdmitService;
 
 @Controller
 public class SlipAdminController {
+	@Autowired
+	SlipAdmitService slipAdmitService;
 	//전표승인
 	@GetMapping("/acc/slipAdmitView")
 	public String slipAdmitView() {
+		System.out.println("slipAdmitView SlipAdminController.java");
 		return "/account/slip/admit/slipadmit_in";
+	}
+	@GetMapping("/slipAdmitList")
+	public @ResponseBody List<SlipAdmit> slipAdmitList(SlipAdmit slipAdmit){
+		System.out.println("slipAdmitList SlipAdminController.java");
+		System.out.println(slipAdmit+"<--전표헤더!! Controller");
+		List<SlipAdmit> slipList = slipAdmitService.slipAdmitList(slipAdmit);
+		System.out.println(slipList+"<--헤더리턴!! Controller");
+		return slipList;
+	}
+	@GetMapping("/slipAdmitList2")
+	public @ResponseBody List<SlipAdmit> slipAdmitList2(SlipAdmit slipAdmit){
+		System.out.println("slipAdmitList SlipAdminController.java");
+		System.out.println(slipAdmit+"<--전표상세!!! Controller");
+		List<SlipAdmit> slipList = slipAdmitService.slipAdmitList2(slipAdmit);
+		System.out.println(slipList+"<--전표상세리턴!!! Controller");
+		return slipList;
 	}
 }
--- ksmart30/src/main/java/ksmart30/team02/account/mapper/SlipAdmitMapper.java
+++ ksmart30/src/main/java/ksmart30/team02/account/mapper/SlipAdmitMapper.java
@@ -1,5 +1,15 @@
 package ksmart30.team02.account.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import ksmart30.team02.account.domain.SlipAdmit;
+@Mapper
 public interface SlipAdmitMapper {
 
+	List<SlipAdmit> slipAdmitList(SlipAdmit slipAdmit);
+	
+	List<SlipAdmit> slipAdmitList2(SlipAdmit slipAdmit);
+	
 }

8957703f0e2f809ac5b5e66f2611d208fafd14b1
LineageRevolution <yyyun99@naver.com>
2019-04-25 14:27:20 +0900

    [ksmart30_team2 윤예찬]사업성검토 승인 , 승인처리 구현중
    <- Controller ->
    BusinessYesanAdmitController.java
    
    <-Model->
    BusinessYesanAdmitService.java
    BusinessYesanAdmitMapper.java
    BusinessYesanAdmitMapper.xml
    <-View->
    businessYesanAdmitView.html

--- ksmart30/src/main/java/ksmart30/team01/business/controller/BusinessYesanAdmitController.java
+++ ksmart30/src/main/java/ksmart30/team01/business/controller/BusinessYesanAdmitController.java
@@ -1,9 +1,15 @@
 package ksmart30.team01.business.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import ksmart30.team01.business.service.BusinessYesanAdmitService;
 
@@ -17,5 +23,29 @@ public class BusinessYesanAdmitController {
 	public String businessYesanAdmitView(Model model) {	
 		model.addAttribute("list", businessYesanAdmitService.getDeptList());
 		return "/business/businessYesanAdmitView";
+	}
+	@PostMapping("/businessYesanAdmitGetProjectList")
+	public @ResponseBody List<Map<String, String>> businessYesanGetProjectList(@RequestBody Map<String,Object> searchMap){
+		
+		return businessYesanAdmitService.getProjectList(searchMap);
+	}
+	
+	@PostMapping("/businessYesanAdmitGetResultProjectList")
+	public @ResponseBody List<Map<String,String>> businessYesanGetResultProjectList(@RequestBody Map<String,Object> searchMap){
+		System.out.println(searchMap.toString());
+		return businessYesanAdmitService.getResultProjectList(searchMap);
+	}
+	
+	@PostMapping("/businessYesanAdmitGetResultEmpList")
+	public @ResponseBody List<Map<String,String>> businessYesanGetResultEmpList(@RequestBody Map<String,Object> searchMap){
+		System.out.println(searchMap.toString());
+		
+		return businessYesanAdmitService.getResultDeptList(searchMap);
+	}
+	
+	@PostMapping("/businessYesanAdmitGetResultProject")
+	public @ResponseBody Map<String,Object> businessYesanGetResultProject(@RequestBody String PJT_CD){
+		System.out.println(PJT_CD);
+		return businessYesanAdmitService.getResultProject(PJT_CD);
 	}
 }
